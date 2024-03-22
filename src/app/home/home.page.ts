814cce30276b3cec696d0f97fd173b2be19bb0eb
CBPaulS <paulchacon13@gmail.com>
2024-03-22 16:14:25 -0500

    v1

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Database, object, ref } from '@angular/fire/database';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  banio: boolean = false;
+  garaje: boolean = false;
+  cocina: boolean = false;
+  cuarto: boolean = false;
+  sala: boolean = false;
+  vestidor: boolean = false;
+
+  constructor(private database: Database) {}
+
+  ngOnInit() {
+    this.subscribeToRoom('banio');
+    this.subscribeToRoom('garaje');
+    this.subscribeToRoom('cocina');
+    this.subscribeToRoom('cuarto');
+    this.subscribeToRoom('sala');
+    this.subscribeToRoom('vestidor');
+  }
+
+  private subscribeToRoom(roomName: string) {
+    const route = ref(this.database, "/casa/" + roomName);
+    object(route).subscribe(attribute => {
+      (this as any)[roomName] = attribute.snapshot.val();
+    });
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyADk7UnaI3NlEx1sicLSUBz5IfYBsEdDWU',
+    authDomain: 'rtbd-iot-ventanas.firebaseapp.com',
+    projectId: 'rtbd-iot-ventanas',
+    storageBucket: 'rtbd-iot-ventanas.appspot.com',
+    messagingSenderId: '956866070246',
+    appId: '1:956866070246:web:dea18a63b3ea4191ac9d27',
+  }
+};
